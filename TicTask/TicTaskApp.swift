ac794651c42dd414c34b965b7f79b2fa4acf8c27
Ina Burström <inaburstrom@hotmail.com>
2025-02-25 21:16:53 +0100

    Initial Commit

---
+++ TicTask/TicTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTaskApp.swift
+//  TicTask
+//
+//  Created by Ina Burström on 2025-02-25.
+//
+
+import SwiftUI
+
+@main
+struct TicTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicTaskTests/TicTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTaskTests.swift
+//  TicTaskTests
+//
+//  Created by Ina Burström on 2025-02-25.
+//
+
+import Testing
+@testable import TicTask
+
+struct TicTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
