ab6b6e4e5a301a73037f3a14474fc11292c57d40
Ina Burström <inaburstrom@hotmail.com>
2025-02-28 19:37:28 +0100

    Added functions login, logout and registerUser.

---
+++ TicTask/Views/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  TicTask
+//
+//  Created by Ina Burström on 2025-02-28.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
