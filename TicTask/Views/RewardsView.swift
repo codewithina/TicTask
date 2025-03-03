c7a698baeb609d5eb697cdba71206fa97f04f4d1
Ina Burström <inaburstrom@hotmail.com>
2025-03-03 21:11:43 +0100

    Added new file for every view in the app.

---
+++ TicTask/Views/RewardsView.swift
@@ -0,0 +1,18 @@
+//
+//  RewardsView.swift
+//  TicTask
+//
+//  Created by Ina Burström on 2025-03-03.
+//
+
+import SwiftUI
+
+struct RewardsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RewardsView()
+}
