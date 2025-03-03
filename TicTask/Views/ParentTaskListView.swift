3bab75ecfb0c692ef38fa82c9cd3d421343d2723
Ina Burström <inaburstrom@hotmail.com>
2025-03-03 21:11:43 +0100

    Added new file for every view in the app.

---
+++ TicTask/Views/ParentTaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  ParentTaskListView.swift
+//  TicTask
+//
+//  Created by Ina Burström on 2025-03-03.
+//
+
+import SwiftUI
+
+struct ParentTaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParentTaskListView()
+}
