a376c8b60ce69f26f8ebe5cdd8ce12da6379c274
Ina Burström <inaburstrom@hotmail.com>
2025-03-12 23:57:25 +0100

    Added extension to convert hex to swift ui color.

---
+++ TicTask/Extensions/ColorHex.swift
@@ -0,0 +1,23 @@
+//
+//  ColorHex.swift
+//  TicTask
+//
+//  Created by Ina Burström on 2025-03-12.
+//
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#") // Ignore "#" 
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let red = Double((rgb >> 16) & 0xFF) / 255.0
+        let green = Double((rgb >> 8) & 0xFF) / 255.0
+        let blue = Double(rgb & 0xFF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
+
