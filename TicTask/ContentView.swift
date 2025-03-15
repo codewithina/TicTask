2a4b2487a539fbe213fdc621964de7d233535f7b
Ina Burström <inaburstrom@hotmail.com>
2025-03-15 02:17:26 +0100

    Added background to app.

--- TicTask/ContentView.swift
+++ TicTask/ContentView.swift
@@ -11,9 +11,48 @@ struct ContentView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
     @EnvironmentObject var taskViewModel: TaskViewModel
     @EnvironmentObject var rewardViewModel: RewardViewModel
-
+    @EnvironmentObject var notificationViewModel: NotificationViewModel
+    
     var body: some View {
         if authViewModel.isAuthenticated {
+            ZStack {
+                
+                Circle()
+                    .fill(Color.purple.opacity(0.3))
+                    .frame(width: 200, height: 200)
+                    .offset(x: -100, y: -200)
+                    .blur(radius: 40)
+                
+                Circle()
+                    .fill(Color.yellow.opacity(0.3))
+                    .frame(width: 150, height: 150)
+                    .offset(x: 150, y: 200)
+                    .blur(radius: 40)
+                
+                Circle()
+                    .fill(Color.blue.opacity(0.3))
+                    .frame(width: 180, height: 180)
+                    .offset(x: -120, y: 150)
+                    .blur(radius: 50)
+                
+                Circle()
+                    .fill(Color.pink.opacity(0.3))
+                    .frame(width: 220, height: 220)
+                    .offset(x: 80, y: -250)
+                    .blur(radius: 50)
+                
+                Circle()
+                    .fill(Color.green.opacity(0.3))
+                    .frame(width: 170, height: 170)
+                    .offset(x: 200, y: -100)
+                    .blur(radius: 45)
+                
+                Circle()
+                    .fill(Color.orange.opacity(0.3))
+                    .frame(width: 140, height: 140)
+                    .offset(x: -200, y: 250)
+                    .blur(radius: 35)
+            }
             MainTabView()
         } else {
             LoginView()
