ee840b9196848a04250f5a104a68eb32fb6979fb
Ina Burström <inaburstrom@hotmail.com>
2025-03-15 02:28:26 +0100

    Adding reward summary view to show count.

--- TicTask/Views/Dashboard/RewardsSummaryView.swift
+++ TicTask/Views/Dashboard/RewardsSummaryView.swift
@@ -1,7 +1,21 @@
 //
-//  Untitled.swift
+//  RewardsSummaryView.swift
 //  TicTask
 //
 //  Created by Ina Burström on 2025-03-14.
 //
 
+import SwiftUI
+
+struct RewardsSummaryView: View {
+    var rewardCount: Int
+
+    var body: some View {
+        SectionBox(title: "Belöningar tillgängliga") {
+            Text("\(rewardCount) belöningar att köpa")
+                .font(.headline)
+                .foregroundColor(.blue)
+                .padding()
+        }
+    }
+}
