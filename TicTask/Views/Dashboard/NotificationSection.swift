a53cef9850771a3c5562d9d5bd1a549eaa214da0
Ina Burström <inaburstrom@hotmail.com>
2025-03-15 03:25:30 +0100

    Handle all of notification section.

--- TicTask/Views/Dashboard/NotificationSection.swift
+++ TicTask/Views/Dashboard/NotificationSection.swift
@@ -1,8 +1,42 @@
 //
-//  RewardsSummaryView.swift
+//  NotificationSection.swift
 //  TicTask
 //
 //  Created by Ina Burström on 2025-03-14.
 //
 
+import SwiftUI
+
+struct NotificationSection: View {
+    @EnvironmentObject var authViewModel: AuthViewModel
+    @EnvironmentObject var notificationViewModel: NotificationViewModel
+    
+    var body: some View {
+        SectionBox(title: "Senaste händelser") {
+            let notifications = notificationViewModel.notifications
+            
+            if !notifications.isEmpty {
+                ForEach(notifications) { notification in
+                    NotificationRow(notification: notification)
+                }
+            } else {
+                Text("Inga nya notiser")
+                    .foregroundColor(.gray)
+                    .padding()
+            }
+            
+           /* if !notifications.isEmpty {
+                Button("Rensa alla") {
+                    if let userID = authViewModel.user?.id {
+                        for notification in notifications {
+                            notificationViewModel.deleteNotification(for: userID, notificationID: notification.id ?? "")
+                        }
+                    }
+                }
+                .foregroundColor(.red)
+                .padding(.top, 5)
+            }*/
+        }
+    }
+}
 
